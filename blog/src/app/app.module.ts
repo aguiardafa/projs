55db0b16fa8cfccc3d4afc15189e900cf2d3d375
logonrm <logonrm@fiap.com.br>
2017-06-19 23:03:49 -0300

    commit aula 3 - dia 19.06.2017

---
+++ blog/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ContatoComponent } from './contato/contato.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { HttpModule } from '@angular/http';
+import { HomeComponent } from './home/home.component';
+import { ArtigoComponent } from './artigo/artigo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContatoComponent,
+    HomeComponent,
+    ArtigoComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ blog/src/app/home/home.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, NgZone } from '@angular/core';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  //declarando variável
+  public artigos = new Array<Artigo>();
+  //construtor da classe
+  constructor(
+    private http: Http,
+    private zone: NgZone) { }
+
+  ngOnInit(): void {
+    this.http.get('/api/artigos')
+      .subscribe(artigos => {
+        this.zone.run(() => {
+          this.resultadoArtigos(artigos);
+        });
+      });
+  }
+  resultadoArtigos(artigos) {
+    this.artigos = JSON.parse(artigos._body);
+  }
+}
+export class Artigo{
+  id:number;
+  titulo:string;
+  resumo:string;
+}
